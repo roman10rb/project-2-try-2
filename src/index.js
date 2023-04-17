958b6821a21df0ed679964ac513374a27fbfbc23
roman10rb <roma10rb@gmail.com>
2023-04-17 19:53:35 +0300

    2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './js/header-theme.js';
 import './js/render-hp-default-markup.js';
 import './js/support.js';
 // import './js/toggle-theme.js';
-import './js/categories.js';
-import './js/mobile-menu.js';
-import './js/modalWindow.js';
+// import './js/categories.js';
+// import './js/mobile-menu.js';
+// import './js/modalWindow.js';
 
 //Приклад використання промісів з getBooksData.js
 // import { getBookData } from './js/getBooksData.js';
